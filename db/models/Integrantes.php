24c83861506d24f479d074398a7497101a99ac32
Od@ <elmorochez22@gmail.com>
2017-05-04 16:20:34 -0400

    agregado las validaciones de municipio y parroquia y tambien si es el mismo id de boega

--- db/models/Integrantes.php
+++ db/models/Integrantes.php
@@ -7,7 +7,7 @@ class Integrantes extends Model {
 	public $timestamps = false;
     protected $table = 'integrantes_clap';
 	protected $primaryKey = 'id';
-    protected $fillable = ['sector_id','zona_id','tipo_c','cedula','e_cadip','e_clap','nombre_a','telefono','jefe_carga','cod_bodega','tipo_b','rif_b','razon_social','municipio','parroquia','tipo_r','cedula_r','responsable','telefono_r','eliminar','cargo_clap','municipio_int','parroquia_int'];
+    protected $fillable = ['sector_id','zona_id','tipo_c','cedula','e_cadip','e_clap','c_municipio','c_parroquia','nombre_a','telefono','jefe_carga','cod_bodega','tipo_b','rif_b','razon_social','municipio','parroquia','tipo_r','cedula_r','responsable','telefono_r','eliminar','cargo_clap','municipio_int','parroquia_int'];
     //Ejemplo de definir campos
 
 	public function sector()
